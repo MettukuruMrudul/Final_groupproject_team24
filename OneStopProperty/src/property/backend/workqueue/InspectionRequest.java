d6eb92fbd0207340ba719b582f46d3447e73f63e
Niharika-Neu <thippeswamy.n@northeastern.edu>
2025-12-07 17:27:08 -0500

    Created HousingInspector , Maintenancerequest and Inspectionrequest

---
+++ OneStopProperty/src/property/backend/workqueue/InspectionRequest.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package property.backend.workqueue;
+
+import java.io.Serializable;
+import java.util.Date;
+import property.backend.users.Property;
+import property.backend.workqueue.WorkRequest;
+
+/**
+ *
+ * @author sruth
+ */
+public class InspectionRequest extends WorkRequest implements Serializable {
+    // Add a serialVersionUID to maintain compatibility between versions
+    private static final long serialVersionUID = 1L;
+    private Property property;
+    private InspectionType type;
+    private Date preferredDate;
+    private String inspectionReport;
+    private boolean passed;
+    
+    public enum InspectionType {
+        ROUTINE("Routine Inspection"),
+        COMPLIANCE("Compliance Inspection"),
+        DAMAGE("Damage Assessment"),
+        PRE_PURCHASE("Pre-Purchase Inspection"),
+        MOVE_IN("Move-In Inspection"),
+        MOVE_OUT("Move-Out Inspection");
+        
+        private String value;
+        
+        private InspectionType(String value) {
+            this.value = value;
+        }
+        
+        public String getValue() {
+            return value;
+        }
+        
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
+    
+    public InspectionRequest() {
+        super();
+    }
+    
+    public Property getProperty() {
+        return property;
+    }
+    
+    public void setProperty(Property property) {
+        this.property = property;
+    }
+    
+    public InspectionType getType() {
+        return type;
+    }
+    
+    public void setType(InspectionType type) {
+        this.type = type;
+    }
+    
+    public Date getPreferredDate() {
+        return preferredDate;
+    }
+    
+    public void setPreferredDate(Date preferredDate) {
+        this.preferredDate = preferredDate;
+    }
+    
+    public String getInspectionReport() {
+        return inspectionReport;
+    }
+    
+    public void setInspectionReport(String inspectionReport) {
+        this.inspectionReport = inspectionReport;
+    }
+    
+    public boolean isPassed() {
+        return passed;
+    }
+    
+    public void setPassed(boolean passed) {
+        this.passed = passed;
+    }
+    
+}
---
+++ OneStopProperty/src/property/backend/workqueue/MaintainceRequest.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package property.backend.workqueue;
+
+import java.io.Serializable;
+import java.util.Date;
+import property.backend.users.Property;
+import property.backend.workqueue.WorkRequest;
+
+/**
+ *
+ * @author sruth
+ */
+public class MaintainceRequest extends WorkRequest implements Serializable {
+    // Add a serialVersionUID to maintain compatibility between versions
+    private static final long serialVersionUID = 1L;
+    private Property property;
+    private String description;
+    private MaintainceType type;
+    private int priority; // 1-5, 5 being highest
+    private Date scheduledDate;
+    private String workerStatus;
+    
+    public enum MaintainceType {
+        PLUMBING("Plumbing"),
+        ELECTRICAL("Electrical"),
+        HVAC("HVAC"),
+        STRUCTURAL("Structural"),
+        APPLIANCE("Appliance"),
+        GENERAL("General");
+        
+        private String value;
+        
+        private MaintainceType(String value) {
+            this.value = value;
+        }
+        
+        public String getValue() {
+            return value;
+        }
+        
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
+    
+    public String getWorkerStatus() {
+    return workerStatus;
+}
+
+    public void setWorkerStatus(String workerStatus) {
+    this.workerStatus = workerStatus;
+}
+
+    
+    public MaintainceRequest() {
+        super();
+        priority = 3; // Default priority
+    }
+    
+    public Property getProperty() {
+        return property;
+    }
+    
+    public void setProperty(Property property) {
+        this.property = property;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public MaintainceType getType() {
+        return type;
+    }
+    
+    public void setType(MaintainceType type) {
+        this.type = type;
+    }
+    
+    public int getPriority() {
+        return priority;
+    }
+    
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+    
+    public Date getScheduledDate() {
+        return scheduledDate;
+    }
+    
+    public void setScheduledDate(Date scheduledDate) {
+        this.scheduledDate = scheduledDate;
+    }
+    
+    
+    
+}
