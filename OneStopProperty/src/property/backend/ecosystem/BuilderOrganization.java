49de64e5a41bf12f482326e953e3814f477c4bc5
Meghana Thota <thota.meg@northeastern.edu>
2025-12-07 19:01:06 -0500

    updated

---
+++ OneStopProperty/src/property/backend/ecosystem/BuilderOrganization.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package property.backend.ecosystem;
+
+import java.io.Serializable;
+import property.backend.ecosystem.Enterprise;
+import property.backend.ecosystem.Organization;
+import property.backend.users.Builder;
+
+/**
+ *
+ * @author Meghana
+ */
+public class BuilderOrganization extends Organization implements Serializable {
+    // Add a serialVersionUID to maintain compatibility between versions
+    private static final long serialVersionUID = 1L;
+    
+    public BuilderOrganization(String name, Enterprise enterprise) {
+        super(name, enterprise, OrganizationType.PROPERTY_FIRM);
+    }
+    
+    @Override
+    protected void addSupportedRoles() {
+        addSupportedRole(new Builder());
+    }
+}
